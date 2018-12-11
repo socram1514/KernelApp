2586eaa05e5566a6a27e58d92cc1b9221abd6156
Marcos Antonio Ramirez <maramirez@stp.gov.py>
2018-12-11 11:21:08 -0300

    List Fragments

--- kernel/src/main/java/com/innovandoapps/library/kernel/model/ItemMenu.java
+++ kernel/src/main/java/com/innovandoapps/library/kernel/model/ItemMenu.java
@@ -2,31 +2,52 @@ package com.innovandoapps.library.kernel.model;
 
 public class ItemMenu {
 
-    private int imagen;
-    private String titulo;
-    private String subTititulo;
+    private final int imagen;
+    private final String titulo;
+    private final String subTititulo;
+
+    private ItemMenu(ItemMenuBuilder builder) {
+        this.imagen = builder.imagen;
+        this.titulo = builder.titulo;
+        this.subTititulo = builder.subTititulo;
+    }
 
     public int getImagen() {
         return imagen;
-    }
-
-    public void setImagen(int imagen) {
-        this.imagen = imagen;
     }
 
     public String getTitulo() {
         return titulo;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
     public String getSubTititulo() {
         return subTititulo;
     }
 
-    public void setSubTititulo(String subTititulo) {
-        this.subTititulo = subTititulo;
+    public static class ItemMenuBuilder{
+        private int imagen;
+        private String titulo;
+        private String subTititulo;
+
+        public ItemMenuBuilder() {}
+
+        public ItemMenuBuilder imagen(int imagen){
+            this.imagen = imagen;
+            return this;
+        }
+
+        public ItemMenuBuilder titulo(String titulo){
+            this.titulo = titulo;
+            return this;
+        }
+
+        public ItemMenuBuilder subTititulo(String subTititulo){
+            this.subTititulo = subTititulo;
+            return this;
+        }
+
+        public ItemMenu build(){
+            return new ItemMenu(this);
+        }
     }
 }
